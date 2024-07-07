a60009940d9d2dbbb389ee0f693623e487ab1901
yusuf <[degzer12@gmail.com]>
2024-07-07 15:23:58 +0500

    section 2 compleated

--- src/app/components/HomeSections/HomeSection2/index.tsx
+++ src/app/components/HomeSections/HomeSection2/index.tsx
@@ -1,19 +1,12 @@
-import Link from "next/link";
+import image from '../../../images/banners/b.png';
 
-const index = () => {
+const HomeSection2 = () => {
     return (
-        <div className="container mx-auto">
-            <div className="flex justify-between items-center">
-                <div className="w-[550px]">
-                    <p className="text-[#70737C] text-[14px]">OUR PRODUCTS</p>
-                    <h2 className="text-[#0D0D0D] text-[40px] font-bold">Our Products</h2>
-                </div>
-                <Link href="/products">
-                    <p className="text-[#359740] text-[14px]">VIEW ALL</p>
-                </Link>
-            </div>
+        <div style={{ backgroundImage: `url(${image.src})`, borderRadius: '40px' }} className="container mx-auto h-[105px] my-[16px] flex flex-col items-center justify-center">
+            <h3 className='text-[#1F2533] text-[24px] font-bold'>We sell seeds </h3>
+            <p className='text-[#70737C] text-[16px] font-semibold'>that always sprout and gardening supplies which never break</p>
         </div>
     );
 }
 
-export default index;
+export default HomeSection2;
--- src/app/components/HomeSections/index.tsx
+++ src/app/components/HomeSections/index.tsx
@@ -1,11 +1,11 @@
 import HomeSection1 from "./HomeSection1";
+import HomeSection2 from './HomeSection2/index';
 
 const HomeSections = () => {
     return (
         <div>
             <HomeSection1 />
-            {/* <HomeSection2 />
-            <HomeSection3 /> */}
+            <HomeSection2 />
         </div>
     );
 }
